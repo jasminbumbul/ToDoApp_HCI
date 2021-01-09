0e830524bc135ea5db516684337fa813d4d62890
Jasmin Bumbul <jasmin.bumbul@edu.fit.ba>
2021-01-09 12:36:04 +0100

    implementirano dodavanje naslova i datuma(odabrati datum ili bez datuma) uz todo stavku

--- dodavanjeTodoItema.js
+++ dodavanjeTodoItema.js
@@ -2,7 +2,9 @@
 //selectors
 const todoInput= document.querySelector('.todo-input');
 const todoButton= document.querySelector('.todo-button');
-
+const todoTitle = document.querySelector('.todo-title');
+const datePicker = document.querySelector('.datePicker');
+const nodateChk= document.querySelector("#checkbox_nodate");
 
 //event listeners
 
@@ -20,18 +22,49 @@ todoButton.addEventListener('click', addTodo);
 //functions
 
 function addTodo(event) {
+    let bool=1;
+    let year= datePicker.value.substring(0,4);
+    let month= datePicker.value.substring(5,7);
+    let day= datePicker.value.substring(8,10);
+    
 
-    if (todoInput.value == "") {
-        alert("You have not entered anything to do!");
+    if (todoTitle.value == "") {
+        alert("Niste unijeli naslov stavke");
+        bool=0;
     }
-    else {
+    if (todoInput.value == "") {
+        alert("Niste unijeli opis za todo stavku");
+        bool=0
+    }
+    if(bool==1) {
         //prevents from reloading the page, because the submit button is clicked
         event.preventDefault();
-   
-        saveToLocalStorage(todoInput.value);
-        
+        const date=[{
+            day: null,
+            month: null,
+            year: null
+        }];
+        if(nodateChk.checked)
+        {
+            date.day=null;
+            date.month=null;
+            date.year=null;
+        }
+        else{
+            date.day=day;
+            date.month=month;
+            date.year=year;
+        }
+        const arr = [
+            {
+                title: todoTitle.value,
+                todo: todoInput.value,
+                date: { day: date.day, month: date.month, year: date.year}
+            }];
+        saveToLocalStorage(arr[0]);
+
         alert("Uspješno dodana todo stavka");
-        location.href= "/danas.html";
+        location.href = "/danas.html";
     }
 }
 
