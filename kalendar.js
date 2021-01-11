5b8321b433949b6aa4485de19ee55eb323b22fea
Ramiz Mašić <ramiz.masic@edu.fit.ba>
2021-01-11 22:31:38 +0100

    page zabiljeske, kalendar oznaceni azurira todos

--- kalendar.js
+++ kalendar.js
@@ -22,6 +22,14 @@ idiPocetna.addEventListener('click', idiPocetnaRedirect);
 function idiPocetnaRedirect() {
     location.href = "/pocetna.html";
 }
+
+function aktiviraj(id){
+    var element=document.getElementById(id);
+    $('span.active').removeClass('active');
+    element.classList.add("active");
+    todoList.innerHTML="";
+    getTodos();
+}
 function addTodaysDate() {
     var date = new Date();
     var dayNumber = date.getDay();
@@ -31,9 +39,7 @@ function addTodaysDate() {
     let daniUSedmici = ['Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota', 'Nedjelja'];
     // todaysdate.innerHTML=daniUSedmici[dayNumber-1]+", "+ mjesec[mjeseciUGodini-1];
     todaysdate.innerHTML = daniUSedmici[dayNumber - 1] + ", " + danUMjesecu + ". " + mjeseciUGodini[mjesec];
-    
-
-
+   
     var element=document.getElementById(danUMjesecu);
     element.classList.add("active");
 }
@@ -46,47 +52,51 @@ function getTodos() {
         todos = JSON.parse(localStorage.getItem("todos"));
     }
     todos.forEach(function (todo) {
-        //todo div creation
-        const todoDiv = document.createElement('div');
-        todoDiv.classList.add('todo');
-        //title creation
-        const title = document.createElement('div');
-        title.classList.add('todo-title');
-        todoDiv.appendChild(title);
-        title.innerText=todo.title;
-        //li creation
-        const newTodo = document.createElement('li');
-        newTodo.classList.add('todo-item');
-        newTodo.innerText = todo.todo;
-        //appends newTodo item to the main div
-        todoDiv.appendChild(newTodo);
-        //date creation
-        const date = document.createElement('div');
-        date.classList.add('todo-date');
-        todoDiv.appendChild(date);
-        if(todo.date.day==null)
-        {
-            date.innerText="Date not set";
+        // var datum = new Date();
+        // var danUMjesecu = datum.getDate();
+        const oznaceni= document.querySelector(".active");
+        if (/*todo.date.day == danUMjesecu || */todo.date.day == oznaceni.id) {
+            //todo div creation
+            const todoDiv = document.createElement('div');
+            todoDiv.classList.add('todo');
+            //title creation
+            const title = document.createElement('div');
+            title.classList.add('todo-title');
+            todoDiv.appendChild(title);
+            title.innerText = todo.title;
+            //li creation
+            const newTodo = document.createElement('li');
+            newTodo.classList.add('todo-item');
+            newTodo.innerText = todo.todo;
+            //appends newTodo item to the main div
+            todoDiv.appendChild(newTodo);
+            //date creation
+            const date = document.createElement('div');
+            date.classList.add('todo-date');
+            todoDiv.appendChild(date);
+            if (todo.date.day == null) {
+                date.innerText = "Date not set";
+            }
+            else {
+                date.innerText = todo.date.day + "." + todo.date.month + "." + todo.date.year + ".";
+            }
+            //category creation
+            const category = document.createElement('div');
+            category.classList.add('todo-categ');
+            todoDiv.appendChild(category);
+            category.innerText = todo.kategorija;
+            //check button
+            // const completedButton = document.createElement('button');
+            // completedButton.innerHTML = '<i class="fas fa-check" ></i>';
+            // completedButton.classList.add("complete-btn");
+            // todoDiv.appendChild(completedButton);
+            // //delete button
+            // const trashButton = document.createElement('button');
+            // trashButton.innerHTML = '<i class="fas fa-trash" ></i>';
+            // trashButton.classList.add("trash-btn");
+            // todoDiv.appendChild(trashButton);
+            //append to list
+            todoList.appendChild(todoDiv);
         }
-        else{
-            date.innerText=todo.date.day+"."+todo.date.month+"."+todo.date.year+".";
-        }
-        //category creation
-        const category = document.createElement('div');
-        category.classList.add('todo-categ');
-        todoDiv.appendChild(category);
-        category.innerText=todo.kategorija;
-        //check button
-        // const completedButton = document.createElement('button');
-        // completedButton.innerHTML = '<i class="fas fa-check" ></i>';
-        // completedButton.classList.add("complete-btn");
-        // todoDiv.appendChild(completedButton);
-        // //delete button
-        // const trashButton = document.createElement('button');
-        // trashButton.innerHTML = '<i class="fas fa-trash" ></i>';
-        // trashButton.classList.add("trash-btn");
-        // todoDiv.appendChild(trashButton);
-        //append to list
-        todoList.appendChild(todoDiv);
     });
 }
--- zabiljeske.js
+++ zabiljeske.js
@@ -1,5 +1,41 @@
-const vratinazad = document.querySelector("#novaZabiljeska");
-vratinazad.addEventListener('click', vratiNazadRedirect);
-function vratiNazadRedirect() {
+const zabList = document.querySelector('.zab-list');
+const dodajNoviBtn = document.querySelector("#dodajZab");
+
+document.addEventListener('DOMContentLoaded', getZabiljeske);
+
+dodajNoviBtn.addEventListener('click', dodajNoviRedirect);
+
+function dodajNoviRedirect() {
+    location.href = "/dodavanjeZabiljeske.html";
+}
+const idiDanas = document.querySelector("#danas");
+idiDanas.addEventListener('click', idiDanasRedirect);
+function idiDanasRedirect() {
     location.href = "/danas.html";
+}
+
+function getZabiljeske() {
+    let zabiljeske;
+    if (localStorage.getItem("zabiljeske") === null) {
+        zabiljeske = [];
+    } else {
+        zabiljeske = JSON.parse(localStorage.getItem("zabiljeske"));
+    }
+    zabiljeske.forEach(function (zab) {
+        //todo div creation
+        const zabDiv = document.createElement('div');
+        zabDiv.classList.add('card');
+        //header creation
+        const header = document.createElement('h5');
+        header.classList.add('card-header');
+        zabDiv.appendChild(header);
+        header.innerText=zab.header;
+        //div zabiljeska text
+        const zabTextdiv = document.createElement('div');
+        zabTextdiv.classList.add('card-body');
+        zabDiv.appendChild(zabTextdiv);
+        //zabiljeska text
+        zabTextdiv.innerHTML = zab.text;
+        zabList.appendChild(zabDiv);
+    });
 }
